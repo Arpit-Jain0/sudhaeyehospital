5913798af8b4f26b2111e3ce900bb39cb293f141
Arpit Jain <YOUR_GITHUB_EMAIL>
2025-09-10 01:47:38 +0530

    Initial commit after clean restart

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'sudhaeyehospital.in' }],
+        destination: 'https://www.sudhaeyehospital.in',
+        permanent: true,
+      },
+    ];
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
